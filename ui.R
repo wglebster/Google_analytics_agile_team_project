b3e96f2068a8000415ebf39e8d2fe1cf1a1f6624
wglebster <wglebster@gmail.com>
2020-08-17 17:16:32 +0100

    complete UI skeleton

--- ui.R
+++ ui.R
@@ -1,4 +1,32 @@
 ui <- fluidPage(
-  titlePanel("Events Dashboard")
+  titlePanel("Events Dashboard"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      dateRangeInput("dates",
+                     label = "Date Range"
+                     ),
+      radioButtons("course",
+                   label = "Select Course",
+                   choices = list("Data Analysis", "Software Development"),
+                   selected = "Data Analysis")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Source"),
+        tabPanel("Landing"),
+        tabPanel("Previous to Goal"),
+        tabPanel("Drop off/Exit",
+                 radioButtons("goal_or_not",
+                              label = "Select",
+                              choices = list("Event clicked", "Event NOT clicked"),
+                              selected = "Event Clicked",
+                              inline = TRUE)
+                 )
+      )
+    )
+    
+    
+  )
 )
 
